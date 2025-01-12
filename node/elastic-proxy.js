ab0dcd75b1f9303bb6905f21178d31a19714d54f
Rohit Agarwal <madudeyo@gmail.com>
2025-01-12 20:12:56 +0530

    fix: Update Elasticsearch host to localhost in delete-old and elastic-proxy files

--- node/elastic-proxy.js
+++ node/elastic-proxy.js
@@ -3,7 +3,7 @@ let app = express();
 let elasticsearch = require('elasticsearch');
 
 let es = new elasticsearch.Client({
-    host: '172.26.12.192:9200',
+    host: 'localhost:9200',
     log: 'trace'
 });
 
