0674bb313a222c9e4fa49467e197cbabf2df3e3f
Rohit Agarwal <madudeyo@gmail.com>
2025-04-26 16:57:44 +0530

    chore: Add whitenoise middleware for improved security and performance

--- dj/settings.py
+++ dj/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -53,7 +54,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "dj.urls"
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 TEMPLATES = [
     {
